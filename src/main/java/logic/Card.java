43d55d120562b2e356496b20596d343a95002534
powerex <flaxazbest@gmail.com>
2018-06-02 00:11:56 +0300

    - modif Card - add class Deck

--- src/main/java/logic/Card.java
+++ src/main/java/logic/Card.java
@@ -2,25 +2,31 @@ package logic;
 
 public class Card {
     private Suit suit;
-    private short rank;
+    private int rank;
 
     private static char[] ranks = {'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'};
+    private static Suit[] suits = {Suit.HEARTS, Suit.SPADES, Suit.DIAMONDS, Suit.CLUBS};
 
     public Card(Suit suit, short rank) {
         this.suit = suit;
         this.rank = rank;
     }
 
+    public Card(int n) {
+        this.suit = suits[n / 13];
+        this.rank = n % 13;
+    }
+
     @Override
     public String toString() {
-        return "" + rank + suit;
+        return "" + ranks[rank] + suit;
     }
 
     public Suit getSuit() {
         return suit;
     }
 
-    public short getRank() {
+    public int getRank() {
         return rank;
     }
 }
